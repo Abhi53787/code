b370b1395d13b2eeeb0336e678a4dd7346a40135
abhi77 <110686058+Abhi53787@users.noreply.github.com>
2022-09-10 12:16:12 +0530

    practice

---
+++ functions.py
@@ -0,0 +1,14 @@
+def hai(name,age,country="India"):
+    print("username",name)
+    print("good morning",name)
+    print("your age",age)
+    print("COUNTRY",country)
+hai("phlip",20,"cannon")
+def multi(a,b):
+    result=  (a) * (b)
+    print(result)
+
+multi(5,7)
+hai('kjhjg',15)
+
+ 
